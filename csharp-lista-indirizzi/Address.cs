c03b885dca32c9982b86f33ac61ae72da927bc7f
FerrariMarc0 <marco1010@live.it>
2023-09-14 17:44:05 +0200

    using-streamreader

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Address
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZipCode { get; set; }
+
+       
+
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = "C:\\Users\\MarcoF\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv";
+
+            List<Address> addressesList = new();
+            try
+            {
+                using (StreamReader read = new(filePath))
+                {
+                    string line;
+
+                    while ((line = read.ReadLine()) != null)
+                    {
+                        string[] col = line.Split(",");
+
+                        if (col.Length >= 6)
+                        {
+                            Address address = new Address
+                            {
+                                Name = col[0],
+                                LastName = col[1],
+                                Street = col[2],
+                                City = col[3],
+                                Province = col[4],
+                                ZipCode = col[5]
+                            };
+                            addressesList.Add(address);
+                        }
+                        else
+                        {
+                            if (col.Length == 1 && string.IsNullOrEmpty(col[0]))
+                            {
+                                Console.WriteLine("Indirizzo mancante\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Dato mancante o inesatto: {line}\n ");
+                            }
+                        }
+                    }
+                }
+                foreach( Address address in addressesList )
+                {
+                    Console.WriteLine( $"Nome: {address.Name}\nCognome: {address.LastName}\nIndirizzo: {address.Street}\nCittà: {address.City}\nProvincia: {address.Province}\nCAP: {address.ZipCode}\n" );
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Si è verificato un errore: {e.Message}");
+            }
         }
     }
 }
